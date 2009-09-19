5a3e157ed7e408f23a22e30841376bba28699be2
kpdecker <kpdecker@gmail.com>
2009-09-19 03:41:43 +0000

    Update attribute iterator to handle removed properties and move into a public namespace.
    git-svn-id: http://fbug.googlecode.com/svn/diff/branches/firediff1.0@4352 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- content/firediff/domplate.js
+++ content/firediff/domplate.js
@@ -75,6 +75,67 @@ function RemovedIterator(content, removed, includeFilter) {
 }
 
 var DomUtil = {
+  getAttributes: function(change) {
+    var attrs = [], attrSeen = {};
+    var idAttr, classAttr, changeAttr;
+    var el = change.clone || change;
+
+    var changes = el[FireDiff.events.AnnotateAttrs.ATTR_CHANGES] || {};
+    if (change.clone) {
+      changes = {};
+      changes[change.attrName] = change;
+    }
+
+    if (el.attributes) {
+      for (var i = 0; i < el.attributes.length; ++i) {
+        var attr = el.attributes[i];
+        if (attr.localName.indexOf("firebug-") != -1)
+           continue;
+
+        // We need to include the change object as domplate does not have an easy way
+        // to pass multiple arguments to a processing method
+        var curChange = changes[attr.localName];
+        if (curChange) {
+            changeAttr = {
+                localName: attr.localName,
+                nodeValue: attr.nodeValue,
+                change: curChange
+            };
+            attr = changeAttr;
+        }
+
+        attrSeen[attr.localName] = true;
+        if (attr.localName == "id") {
+          idAttr = attr;
+        }
+        else if (attr.localName == "class") {
+         classAttr = attr;
+        }
+        else {
+          attrs.push(attr);
+        }
+      }
+    }
+    if (classAttr) {
+      attrs.splice(0, 0, classAttr);
+    }
+    if (idAttr) {
+      attrs.splice(0, 0, idAttr);
+    }
+
+    // Handle any removed attributes
+    for (var i in changes) {
+      if (changes.hasOwnProperty(i) && !attrSeen.hasOwnProperty(i)) {
+        attrs.push({
+            localName: i,
+            nodeValue: "",
+            change: changes[i]
+        });
+      }
+    }
+
+    return attrs;
+  },
   isEmptyElement: function(element) {
     return !element.firstChild && !element[Events.AnnotateAttrs.REMOVE_CHANGES];
   },
@@ -141,56 +202,7 @@ var attributeList = domplate({
   },
 
   attrIterator: function(change) {
-      var attrs = [];
-      var idAttr, classAttr, changeAttr;
-      var el = change.clone || change;
-      
-      var changes = el[FireDiff.events.AnnotateAttrs.ATTR_CHANGES] || {};
-      if (change.clone) {
-        changes = {};
-        changes[change.attrName] = change;
-      }
-      
-      if (el.attributes) {
-          for (var i = 0; i < el.attributes.length; ++i) {
-              var attr = el.attributes[i];
-              
-              if (attr.localName.indexOf("firebug-") != -1)
-                 continue;
-
-              // We need to include the change object as domplate does not have an easy way
-              // to pass multiple arguments to a processing method
-              var curChange = changes[attr.localName];
-              if (curChange) {
-                  changeAttr = {
-                      localName: attr.localName,
-                      nodeValue: attr.nodeValue,
-                      change: curChange
-                  };
-                  attr = changeAttr;
-              }
-              
-              if (attr.localName == "id")
-                  idAttr = attr;
-              else if (attr.localName == "class")
-                 classAttr = attr;
-              else
-                  attrs.push(attr);
-              
-          }
-      }
-      if (classAttr)
-          attrs.splice(0, 0, classAttr);
-      if (idAttr)
-          attrs.splice(0, 0, idAttr);
-      if (!changeAttr && change.attrName) {
-          attrs.push({
-                  localName: change.attrName,
-                  nodeValue: "",
-                  change: change
-              });
-      }
-      return attrs;
+    return DomUtil.getAttributes(change);
   },
 
   isAttrAdded: function(attr) {
