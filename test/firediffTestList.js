51289a2f4d7ebaf4df57094d688b85787ae85487
kpdecker <kpdecker@gmail.com>
2010-01-18 21:46:53 +0000

    Update firediff and fireformat tests to allow merged UTs.
    git-svn-id: http://fbug.googlecode.com/svn/diff/branches/firediff1.0@5735 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ test/firediffTestList.js
@@ -0,0 +1,23 @@
+﻿var testList = testList || [];
+testList.push.apply(testList, [
+    {group: "module", uri: "module/appChangeTest.js",               desc: "Diff Module: Application Generated Events" },
+    {group: "module", uri: "module/domEditorTest.js",               desc: "Diff Module: DOM Editor Generated Events" },
+    {group: "module", uri: "module/cssEditorTest.js",               desc: "Diff Module: CSS Editor Generated Events" },
+
+    {group: "lib",    uri: "lib/jsDiffTest.js",                     desc: "Lib: JS Diff" },
+    {group: "lib",    uri: "lib/pathTest.js",                       desc: "Lib: Custom XPath Test" },
+    {group: "lib",    uri: "lib/cssPathTest.js",                   desc: "Lib: CSS Path Test" },
+    {group: "lib",    uri: "lib/cssModelTest.js",                   desc: "Lib: CSS Model Test" },
+
+    {group: "event",  uri: "event/applyRevertTest.js",              desc: "Events: Apply Revert Test" },
+    {group: "event",  uri: "event/attrMergeTest.js",                desc: "Events: Attribute Change Merge Test" },
+    {group: "event",  uri: "event/charDataMergeTest.js",            desc: "Events: Char Data Change Merge Test" },
+    {group: "event",  uri: "event/insertMergeTest.js",              desc: "Events: Insert Node Merge Test" },
+    {group: "event",  uri: "event/removeMergeTest.js",              desc: "Events: Remove Node Merge Test" },
+    {group: "event",  uri: "event/cssMergeTest.js",                 desc: "Events: CSS Merge Test" },
+    {group: "event",  uri: "event/revertMergeTest.js",                desc: "Events: Revert Merge Test" },
+
+    {group: "snapshot", uri: "snapshot/domDiffWalker.js",          desc: "Snapshot: DOM Diff Walker Test" },
+
+    {group: "manual", uri: "manual/manualAutomation.js",            desc: "Manual: Manual display test with scripted setup" }
+]);
