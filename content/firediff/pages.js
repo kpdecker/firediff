a6d143d46d20b9c893df64a71c101a39f42e6a15
kpdecker <kpdecker@gmail.com>
2010-11-28 21:58:00 +0000

    Move Monitor page into it's own file
    git-svn-id: http://fbug.googlecode.com/svn/diff/branches/firediff1.1@8554 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- content/firediff/pages.js
+++ content/firediff/pages.js
@@ -10,8 +10,6 @@ try {
 } catch (err) {
 }
 
-const dateFormat = CCSV("@mozilla.org/intl/scriptabledateformat;1", "nsIScriptableDateFormat");
-
 var Events = FireDiff.events,
     Path = FireDiff.Path,
     CSSModel = FireDiff.CSSModel,
@@ -19,88 +17,6 @@ var Events = FireDiff.events,
     Search = FireDiff.search;
 
 var i18n = document.getElementById("strings_firediff");
-
-// Object used to define the monitor view
-this.Monitor = domplate({
-  entry: DIV(
-      {class: "diffMonitorElement", $firebugDiff: "$change|isFirebugDiff", $appDiff: "$change|isAppDiff", _repObject: "$change"},
-      SPAN({class: "diffSummary"}, "$change|getSummary"),
-      SPAN({class: "diffSep"}, ":"),
-      SPAN({class: "diffSource"}, "$change|getDiffSource"),
-      SPAN({class: "diffDate"}, "$change|getDate"),
-      DIV({class: "diffXPath"}, "$change|getXPath"),
-      DIV({class: "logEntry"}, TAG("$change|getChangeTag", {change: "$change", object: "$change.target"}))
-      ),
-  
-  getChangeTag: function(change) {
-    if (change.changeType == "CSS") {
-      return DiffDomplate.CSSChanges.CSSStyleRule.tag;
-    } else if (change.clone instanceof Text) {
-      return DiffDomplate.TextChanged.tag;
-    } else {
-      return DiffDomplate.ElementChanged.tag;
-    }
-  },
-  getSummary: function(change) {
-    return change.getSummary();
-  },
-  getDiffSource: function(change) {
-    if (this.isFirebugDiff(change)) {
-      return i18n.getString("source.firebug");
-    } else {
-      return i18n.getString("source.application");
-    }
-  },
-  getDate: function(change) {
-    var date = change.date;
-    return dateFormat.FormatDateTime(
-        "", dateFormat.dateFormatLong, dateFormat.timeFormatSeconds,
-        date.getFullYear(), date.getMonth() + 1, date.getDate(),
-        date.getHours(), date.getMinutes(), date.getSeconds()); 
-  },
-  getXPath: function(change) {
-    return change.displayXPath || change.xpath || "";
-  },
-  isFirebugDiff: function(change) {
-    return change.changeSource == Events.ChangeSource.FIREBUG_CHANGE;
-  },
-  isAppDiff: function(change) {
-    return change.changeSource == Events.ChangeSource.APP_CHANGE;
-  },
-  
-  getChanges: function() {
-    return Firebug.DiffModule.getChanges();
-  },
-  getTag: function(object) {
-    return this.entry;
-  },
-  
-  show: function(panel) {
-    var changes = Firebug.DiffModule.getChanges();
-    for (var i = 0; i < changes.length; i++) {
-      this.onChange(changes[i], panel);
-    }
-  },
-  onChange: function(change, panel) {
-    try {
-      this.entry.append({change: change}, panel.panelNode);
-    } catch (err) {
-      FBTrace.sysout("ERROR: onChange", err);
-    }
-  },
-  search: function(text, reverse, panel) {
-    this.searchHelper = this.searchHelper || new Search.PageSearch();
-    return this.searchHelper.search(text, reverse, panel);
-  }
-});
-this.MonitorRep = domplate(Firebug.Rep,{
-  supportsObject: function(object, type) {
-    return object == FireDiff.reps.Monitor;
-  },
-  getTitle: function(object) {
-    return i18n.getString("page.ChangeLog");
-  }
-});
 
 /**
  * Initializes the base snapshot datastructures. The passed parameter may be a
