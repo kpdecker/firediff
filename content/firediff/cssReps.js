11055a7907612da3447a336670b7c2cb96992fb8
kpdecker <kpdecker@gmail.com>
2010-12-01 02:25:47 +0000

    CSS Clone reps
    git-svn-id: http://fbug.googlecode.com/svn/diff/branches/firediff1.1@8604 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ content/firediff/cssReps.js
@@ -0,0 +1,38 @@
+/* See license.txt for terms of usage */
+
+FBL.ns(function() { with (FBL) {
+
+var Fireformat = {},
+    Formatters;
+
+try {
+  Components.utils.import("resource://fireformat/formatters.jsm", Fireformat);
+  Formatters = Fireformat.Formatters;
+} catch (err) {
+}
+
+var CSSRuleCloneRep = domplate(Firebug.Rep, {
+    supportsObject: function(object, type) {
+        return object instanceof FireDiff.CSSModel.CSSStyleRuleClone
+            || object instanceof FireDiff.CSSModel.CSSMediaRuleClone
+            || object instanceof FireDiff.CSSModel.CSSFontFaceRuleClone
+            || object instanceof FireDiff.CSSModel.CSSImportRuleClone
+            || object instanceof FireDiff.CSSModel.CSSCharsetRuleClone
+            || object instanceof FireDiff.CSSModel.CSSRuleClone;
+    },
+
+    copyRuleDeclaration: function(cssSelector) {
+        copyToClipboard(Formatters.getCSSFormatter().format(cssSelector));
+    },
+
+    getContextMenuItems: function(object, target, context) {
+        if (Formatters) {
+            return [
+                 {label: "Copy Rule Declaration", command: bindFixed(this.copyRuleDeclaration, this, object) },
+            ];
+        }
+    }
+});
+
+Firebug.registerRep(CSSRuleCloneRep);
+}});
