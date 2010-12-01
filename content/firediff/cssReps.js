5fbac9e0eb03fdb32f052a732e280a888fd0a0b3
kpdecker <kpdecker@gmail.com>
2010-12-01 03:16:22 +0000

    Include normal CSS types in the copy rep
    git-svn-id: http://fbug.googlecode.com/svn/diff/branches/firediff1.1@8608 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- content/firediff/cssReps.js
+++ content/firediff/cssReps.js
@@ -12,13 +12,20 @@ try {
 }
 
 var CSSRuleCloneRep = domplate(Firebug.Rep, {
+
     supportsObject: function(object, type) {
         return object instanceof FireDiff.CSSModel.CSSStyleRuleClone
             || object instanceof FireDiff.CSSModel.CSSMediaRuleClone
             || object instanceof FireDiff.CSSModel.CSSFontFaceRuleClone
             || object instanceof FireDiff.CSSModel.CSSImportRuleClone
             || object instanceof FireDiff.CSSModel.CSSCharsetRuleClone
-            || object instanceof FireDiff.CSSModel.CSSRuleClone;
+            || object instanceof FireDiff.CSSModel.CSSRuleClone
+            || object instanceof CSSStyleRule
+            || object instanceof CSSMediaRule
+            || object instanceof CSSFontFaceRule
+            || object instanceof CSSImportRule
+            || object instanceof CSSCharsetRule
+            || object instanceof CSSUnknownRule;
     },
 
     copyRuleDeclaration: function(cssSelector) {
